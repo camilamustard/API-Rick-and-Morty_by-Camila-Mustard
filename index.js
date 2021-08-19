37d243e6f02b2d16669d5f111ca324c2cc1bdc47
Camila Mustard <camivilacadev@gmail.com>
2021-08-19 12:01:33 -0300

    Arquivos

---
+++ index.js
@@ -0,0 +1,22 @@
+//função
+function addtask() {
+    
+}
+//task
+const tasktitle = document.querySelector
+
+//botão
+const addBtn= document.querySelector ("#button");
+function(getInfo){
+    let randomnumber = Math.floor(Math.random() *88 )+1}
+    let apiurl= ('https://swapi.dev/api/people') + randomnumber
+
+    axios.get(apiurl) then (Response.data)}
+catch (e => {
+    updateInfoWithError()})}
+    function updateInfo(data) {
+        name.innertex =data.name
+        height.innertex =         
+    }
+console.log("testando")});
+function getInfo(){}
